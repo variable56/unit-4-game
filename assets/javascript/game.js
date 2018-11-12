8beb4f10b6eff420db82ab1aa7a4e9bc37c1edc5
variable56 <variable56@gmail.com>
2018-11-12 14:02:16 -0700

    working random #'s and score update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,67 @@
 //set variables and randomize gem numbers
 var points = 0;
-var targetPoints = Math.floor(Math.random() * 100);
-var gem1val = Math.floor(Math.random() * 50);
-var gem2val = Math.floor(Math.random() * 10);
-var gem3val = Math.floor(Math.random() * 10);
-var gem4val = Math.floor(Math.random() * 10);
-$("#targetScore").html(targetPoints);
 
-//Create a win scenario
+var gem1val = Math.floor((Math.random() * 11) + 5) * 10;
+var gem2val = Math.floor((Math.random() * 11) + 5) * 10;
+var gem3val = Math.floor((Math.random() * 11) + 5) * 10;
+var gem4val = Math.floor((Math.random() * 11) + 5) * 10;
+var targetPoints = ((gem1val + gem2val + gem3val + gem4val) * 3);
+console.log(gem1val);
+console.log(gem2val);
+console.log(gem3val);
+console.log(gem4val);
+
+//create function for resetting the game when win condition is met
+
+function ifWin() {
+    alert("Congratulations! You win.");
+    points = 0;
+    gem1val = Math.floor((Math.random() * 11) + 5) * 10;
+    gem2val = Math.floor((Math.random() * 11) + 5) * 10;
+    gem3val = Math.floor((Math.random() * 11) + 5) * 10;
+    gem4val = Math.floor((Math.random() * 11) + 5) * 10;
+    targetPoints = ((gem1val + gem2val + gem3val + gem4val) * Math.floor(Math.random() * 3));
+}
+
+
 
 //create function for resetting the game when win condition is met
 
 //set gems to register when clicked and update the DOM
 $(document).ready(function() {
 
+    //Create a win scenario
+
+    if (points === targetPoints) {
+        ifWin();
+    };
+
+    $("#targetScore").html("<h3>Target Points: " + targetPoints);
+
     $("#gem1").on("click", (function() {
-        
+
         points += gem1val;
-        console.log(gem1val);
         $("#totalScore").html("<h3>Total Points: " + points);
         
     }));
 
     $("#gem2").on("click", (function() {
         
-        points += 5;
+        points += gem2val;
         $("#totalScore").html("<h3>Total Points: " + points);
         
     }));
 
     $("#gem3").on("click", (function() {
         
-        points += 20;
+        points += gem3val;
         $("#totalScore").html("<h3>Total Points: " + points);
         
     }));
 
     $("#gem4").on("click", (function() {
         
-        points += 20;
+        points += gem4val;
         $("#totalScore").html("<h3>Total Points: " + points);
         
     }));
