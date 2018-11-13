2238217814890a0da66b637a985622646fed2b1e
variable56 <variable56@gmail.com>
2018-11-12 21:02:22 -0700

    working game, needs styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,69 +1,120 @@
-//set variables and randomize gem numbers
-var points = 0;
+$(document).ready(function () {
+    //set variables and randomize gem numbers
+    var points = 0;
+    var gem1val = Math.floor((Math.random() * 11) + 5) * 10;
+    var gem2val = Math.floor((Math.random() * 11) + 5) * 10;
+    var gem3val = Math.floor((Math.random() * 11) + 5) * 10;
+    var gem4val = Math.floor((Math.random() * 11) + 5) * 10;
+    var targetPoints = Math.floor((Math.random() * 11) + 1) * 100;
+    var wins = 0;
+    var losses = 0;
+    console.log(gem1val);
+    console.log(gem2val);
+    console.log(gem3val);
+    console.log(gem4val);
 
-var gem1val = Math.floor((Math.random() * 11) + 5) * 10;
-var gem2val = Math.floor((Math.random() * 11) + 5) * 10;
-var gem3val = Math.floor((Math.random() * 11) + 5) * 10;
-var gem4val = Math.floor((Math.random() * 11) + 5) * 10;
-var targetPoints = ((gem1val + gem2val + gem3val + gem4val) * 3);
-console.log(gem1val);
-console.log(gem2val);
-console.log(gem3val);
-console.log(gem4val);
+    //create function for resetting the game when win condition is met
 
-//create function for resetting the game when win condition is met
-
-function ifWin() {
-    alert("Congratulations! You win.");
+    function ifWin() {
     points = 0;
-    gem1val = Math.floor((Math.random() * 11) + 5) * 10;
-    gem2val = Math.floor((Math.random() * 11) + 5) * 10;
-    gem3val = Math.floor((Math.random() * 11) + 5) * 10;
-    gem4val = Math.floor((Math.random() * 11) + 5) * 10;
-    targetPoints = ((gem1val + gem2val + gem3val + gem4val) * Math.floor(Math.random() * 3));
-}
+    gem1val = Math.floor((Math.random() * 11) + 1) * 1;        
+    gem2val = Math.floor((Math.random() * 11) + 1) * 5;
+    gem3val = Math.floor((Math.random() * 11) + 1) * 10;
+    gem4val = Math.floor((Math.random() * 11) + 1) * 10;
+    targetPoints = Math.floor((Math.random() * 11) + 1) * 100;
+    $("#targetScore").html("<h3>Target Points: " + targetPoints);
+    $("#totalScore").html("<h3>Total Points: " + points);
+    wins++;
+    $("#wins").html("<h3>Wins: " + wins);
+    
 
+    console.log(gem1val);
+    console.log(gem2val);
+    console.log(gem3val);
+    console.log(gem4val);
+    console.log(targetPoints);
+    alert("You Win! Congratulations!!!")
+    }
 
+    function ifLose() {
+    points = 0;
+    gem1val = Math.floor((Math.random() * 11) + 1) * 5;       
+    gem2val = Math.floor((Math.random() * 11) + 1) * 5;
+    gem3val = Math.floor((Math.random() * 11) + 1) * 5;
+    gem4val = Math.floor((Math.random() * 11) + 1) * 5;
+    targetPoints = Math.floor((Math.random() * 11) + 1) * 100;
+    $("#targetScore").html("<h3>Target Points: " + targetPoints);
+    $("#totalScore").html("<h3>Total Points: " + points);
+    losses++;
+    $("#losses").html("<h3>Losses: " + losses);
+    
 
-//create function for resetting the game when win condition is met
+    console.log(gem1val);
+    console.log(gem2val);
+    console.log(gem3val);
+    console.log(gem4val);
+    console.log(targetPoints);
+    alert("Too bad.  Better luck next time");
+    }
 
-//set gems to register when clicked and update the DOM
-$(document).ready(function() {
-
-    //Create a win scenario
-
-    if (points === targetPoints) {
-        ifWin();
-    };
+    //set gems to register when clicked and update the DOM
 
     $("#targetScore").html("<h3>Target Points: " + targetPoints);
+    $("#wins").html("<h3>Wins: " + wins);
+    $("#losses").html("<h3>Losses: " + losses);
 
-    $("#gem1").on("click", (function() {
+    $("#gem1").on("click", (function () {
 
         points += gem1val;
         $("#totalScore").html("<h3>Total Points: " + points);
-        
+        if (points === targetPoints) {
+            ifWin();
+        }
+        if (points > targetPoints) {
+            ifLose();
+        }
+
     }));
 
-    $("#gem2").on("click", (function() {
-        
+    $("#gem2").on("click", (function () {
+
         points += gem2val;
         $("#totalScore").html("<h3>Total Points: " + points);
+        if (points === targetPoints) {
+            ifWin();
+        }
+        if (points > targetPoints) {
+            ifLose();
+        
+        }
         
     }));
 
-    $("#gem3").on("click", (function() {
-        
+    $("#gem3").on("click", (function () {
+
         points += gem3val;
         $("#totalScore").html("<h3>Total Points: " + points);
+        if (points === targetPoints) {
+            ifWin();
+        }
+        if (points > targetPoints) {
+            ifLose();
+        }
         
     }));
 
-    $("#gem4").on("click", (function() {
-        
+    $("#gem4").on("click", (function () {
+
         points += gem4val;
         $("#totalScore").html("<h3>Total Points: " + points);
+        if (points === targetPoints) {
+            ifWin();
+        }
+        if (points > targetPoints) {
+            ifLose();
+        }
         
     }));
 
 });
+
